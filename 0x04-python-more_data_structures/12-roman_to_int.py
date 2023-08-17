9d0b37abf8ae157d059e7f30a8498f9fdcf8883d
Samuel Eleshinnla <samelesh1@gmail.com>
2023-08-16 19:34:27 -0700

    Commit to tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,39 @@
+#!/usr/bin/python3
 def subtracting(list_num):
-    return max(list_num) - sum(list_num)
+    sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            sub += n
+
+    return (max_list - sub)
 
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string:
         return 0
-    
+
+    if not isinstance(roman_string, str):
+        return 0
+
     roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_num = [0]
+    list_keys = list(roman_num.keys())
+
     number = 0
     last_roman = 0
-    
+    list_num = [0]
+
     for a in roman_string:
-        if a in roman_num:
-            if roman_num[a] <= last_roman:
-                number += subtracting(list_num)
-                list_num = [roman_num[a]]
-            else:
-                list_num.append(roman_num[a])
-            
-            last_roman = roman_num[a]
-    
+        for rn in list_keys:
+            if rn == a:
+                if roman_num.get(a) <= last_roman:
+                    number += subtracting(list_num)
+                    list_num = [roman_num.get(a)]
+                else:
+                    list_num.append(roman_num.get(a))
+
+                last_roman = roman_num.get(a)
+
     number += subtracting(list_num)
-    return number
+
+    return (number)
